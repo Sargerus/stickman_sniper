ab3002fcaee5f0963237eb4c1637e70f33fb54e8
Pavel Kulich <shlakva@mail.ru>
2023-11-30 11:51:19 +0300

    hide mobile screen on lose/win

--- stickman_sniper/Assets/Scripts/ProgressObservers/WinLoseDecider.cs
+++ stickman_sniper/Assets/Scripts/ProgressObservers/WinLoseDecider.cs
@@ -1,4 +1,5 @@
 using Cysharp.Threading.Tasks;
+using DWTools;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,21 +11,24 @@ public class WinLoseDecider : IInitializable, IDisposable
     private readonly IUiManager _uiManager;
     private readonly List<IProgressObserver> _progressObservers;
     private readonly FirstPersonController _firstPersonController;
+    private readonly CameraProvider _cameraProvider;
 
     private CompositeDisposable _disposables = new();
 
     public WinLoseDecider(IUiManager uiManager, List<IProgressObserver> progressObservers,
-        FirstPersonController firstPersonController)
+        FirstPersonController firstPersonController, [Inject(Id ="mobile")] CameraProvider cameraProvider)
     {
         _uiManager = uiManager;
         _progressObservers = progressObservers;
         _firstPersonController = firstPersonController;
+        _cameraProvider = cameraProvider;
     }
 
     public void Initialize()
     {
         Observable.Merge(_progressObservers.Select(g => g.Win)).Where(h => h == true).Subscribe(_ =>
         {
+            HideMobile();
             _firstPersonController.Freeze(true);
             _firstPersonController.ToggleScopeOff();
             _uiManager.ShowWinPopup().Forget();
@@ -32,10 +36,16 @@ public class WinLoseDecider : IInitializable, IDisposable
 
         Observable.Merge(_progressObservers.Select(g => g.Lose)).Where(h => h == true).Subscribe(_ =>
         {
+            HideMobile();
             _firstPersonController.Freeze(true);
             _firstPersonController.ToggleScopeOff();
             _uiManager.ShowLosePopup().Forget();
         }).AddTo(_disposables);
+    }
+
+    private void HideMobile()
+    {
+        _cameraProvider.Camera.gameObject.SetActive(false);
     }
 
     public void Dispose()

064d87a88ca60eb1ce8a0fefd476e30d369b9fab
Pavel Kulich <shlakva@mail.ru>
2024-07-18 09:53:12 +0300

    try fix build

--- stickman_sniper/Assets/Behavior Designer Samples/Common/SampleConstants.cs
+++
@@ -1,9 +0,0 @@
-namespace BehaviorDesigner.Samples
-{
-    // Constant variables used by multiple classes
-    public class SampleConstants
-    {
-        // Any magnitude less than this value and the object will be considered to have arrived at its destination
-        public static float ArriveMagnitude = 1f;
-    }
-}
